c6fb8e03713834077493b67010cb2b908e360350
Riyaz <91373561+Riyaz1000@users.noreply.github.com>
2024-01-08 01:08:25 +0530

    fix:ip address

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-// Write your javascript code here
+//to get this ip address (152.58.214.27) from this url: https://ipv4.icanhazip.com/
+
+let ipAddressEl = document.getElementById("ip");
+
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    // console.log(xhr.responseText);
+    ipAddressEl.textContent = xhr.responseText;
+  }
+};
+
+xhr.open(
+  "GET",
+  "https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/"
+);
+xhr.send();
