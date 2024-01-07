f69a8c46d1402b49d116a2aa697f6aedd5532187
Riyaz <91373561+Riyaz1000@users.noreply.github.com>
2024-01-08 01:11:52 +0530

    update tailwindconfig

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // base: '/YOUR_BASE/'
+  base: "/ipv4-finder/",
   //   build: {
   //     rollupOptions: {
   //       input: {
